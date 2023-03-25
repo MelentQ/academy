edd882d871d18541af6e5fe014d92d593cba7da2
MelentQ <mejleht@yandex.ru>
2023-03-25 18:46:12 +0300

    Updates

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -17,7 +17,7 @@ function handleSubmitForm() {
 
           const name = form.name.value || "Не указано";
           const phone = form.phone.value || "Не указано";
-          const string = getFormDataString(formData);
+          const string = getFormDataString(form, formData);
 
           url += `?name=${encodeURI(name)}&phone=${encodeURI(phone)}&message=${encodeURI(string)}`;
 
@@ -44,10 +44,25 @@ function handleSubmitForm() {
   })
 }
 
-function getFormDataString(formData) {
+function getFormDataString(form, formData) {
   let string = "Данные с формы на сайте: \n";
+
+  const quizFormHiddenInput = form.querySelector('.js-quiz-path');
+  const blockedInputs = [];
+  if (quizFormHiddenInput) {
+      const path = JSON.parse(quizFormHiddenInput.value);
+      const tabs = [...form.querySelectorAll('[data-index]')];
+      const blockedTabs = tabs.filter(item => !path.includes(item.dataset.index));
+      blockedTabs.forEach(tab => {
+          const hiddenInput = tab.querySelector('input[type="hidden"]');
+          blockedInputs.push(hiddenInput.name);
+      })
+  }
+
   formData.forEach(function (value, key) {
-      string += `${key}: ${value} <<<>>>`;
+      if (!blockedInputs.includes(key)) {
+          string += `${key}: ${value} <<<>>>`;
+      }
   });
 
   return string;
