89825261c736f0dda1fd64a98397dcfda89e9734
MelentQ <mejleht@yandex.ru>
2023-05-02 20:42:16 +0300

    fix js

--- src/js/my/marquee.js
+++ src/js/my/marquee.js
@@ -3,7 +3,7 @@ import Marquee3k from 'marquee3000';
 
 export default function marquee() {
   const contentWrapper = document.querySelector('.marquee__text-block');
-  if (trim(contentWrapper.textContent)) {
+  if (contentWrapper && trim(contentWrapper.textContent)) {
     Marquee3k.init({
       selector: 'js-marquee'
     });
